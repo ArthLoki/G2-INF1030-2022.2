88a91f10ee2581ce5e248dcba500ae6376aebc01
Loki <80011623+ArthLoki@users.noreply.github.com>
2022-11-08 22:33:01 -0300

    create index.html, style.css and script.js

---
+++ Javascript/script.js
@@ -0,0 +1,32 @@
+// Canvas global variables
+var cv = document.getElementById("cv");
+var ctx = cv.getContext("2d");
+
+
+// Functions
+
+// Loads the menu and its events
+function menu(){
+    drawMenu();
+}
+
+// draw items in menu
+function drawMenu(){
+    cv.backgroundColor = '#008037';
+}
+
+// function used to start the game by clicking the star button
+function startGame(){}
+
+// function that controls the game ans its loops
+function game(){}
+
+// restarts the game
+function restartGame(){}
+
+// Main function
+function main(){
+    menu();
+}
+
+main();
